1191a21a384c9d208715fc2254986918813e978a
Samarth S <samarth.s2908@gmail.com>
2025-12-28 14:18:47 +0000

    I have already created a Firebase Realtime Database here: https://consol

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,8 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  // This is the critical line that connects to your specific Realtime Database
+  databaseURL: "https://live-bus-tracker-91f94-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
